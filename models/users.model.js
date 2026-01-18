83c30996c501d8f7d6ddb4aa680c1d4f4957b408
avigailrapa <avigailrapa@gmail.com>
2026-01-18 21:31:41 +0200

    add comparePassword method and toJson transform

--- models/users.model.js
+++ models/users.model.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
     username: String,
     email: { type: String, unique: true },
     password: String,
-    phone: String
+    phone: String,
+    role:{type:String,enums:['admin','user'],required:true}
 });
 
 userSchema.pre('save',function(){
@@ -13,7 +14,23 @@ userSchema.pre('save',function(){
   const hash = bcrypt.hashSync(this.password, salt);
   this.password = hash;
 });
+
+userSchema.method('comparePasswords',function(newPassword){
+    const isEqual=bcrypt.compareSync(newPassword,this.password)
+    return isEqual;
+});
+
+userSchema.set('toJSON',{
+    virtuals:true,
+    transform(doc,converted){
+        delete converted.password;
+        delete converted._id;
+        delete converted.__v;
+
+    }
+})
 export default model('User', userSchema);
+
 
 export const validateUser = {
  login: Joi.object({
@@ -22,7 +39,7 @@ export const validateUser = {
     }),
     // user register (username, email, password, repeat_password, phone)
     register: Joi.object({
-        username: Joi.string().alphanum().trim().min(5).required(), // .trim() חיתוך רווחים
+        username: Joi.string().alphanum().trim().min(5).required(), 
         email: Joi.string().email().required(),
         password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
         repeat_password: Joi.ref('password'),
